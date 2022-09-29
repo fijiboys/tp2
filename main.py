a1716fc49b3bae91488de76add4b749506b164c5
Harrisonh <harrisonh@educ.college-montreal.qc.ca>
2022-09-29 09:17:27 -0400

    tp2

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+HENRY HARRISON
+23 SEPTEMBRE 2022
+CODE: DEVINETTE DE CHIFFRE
+
+"""
+import random
+
+nombre = random.randint(0, 100)
+
+nb_essai = 0
+
+print("J'ai choisi un nombre entre 0 et 100.")
+print("a vous de le deviner!")
+
+chiffre_joueur = int(input("Entrez votre essai:  "))
+
+nb_essai = nb_essai + 1
+
+if chiffre_joueur < nombre:
+    print(nombre > chiffre_joueur)
