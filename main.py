7d2a8c68adc1b4d707859312b03b34c408e510f9
Harrisonh <harrisonh@educ.college-montreal.qc.ca>
2022-10-13 09:20:32 -0400

    tp2

--- main.py
+++ main.py
@@ -5,25 +5,40 @@ CODE: DEVINETTE DE CHIFFRE
 
 """
 import random
+boucle_jeu = True
+while boucle_jeu:
+    nombre = random.randint(0, 100)
 
-nombre = random.randint(0, 100)
+    nb_essai = 0
 
-nb_essai = 0
+    print("J'ai choisi un nombre entre 0 et 100.")
+    print("a vous de le deviner!")
 
-print("J'ai choisi un nombre entre 0 et 100.")
-print("a vous de le deviner!")
+    boucle_essai = True
+    while boucle_essai:
+        nb_essai = nb_essai + 1
+        if nb_essai == 100:
+            boucle_essai = False
+            print("le nombre etait",nombre,"tu est pas tres bon si ton nombre d'essai etait 100")
+        print("essai numero", nb_essai)
 
-chiffre_joueur = int(input("Entrez votre essai:  "))
+        chiffre_joueur = int(input("Entrez votre numero choisi:  "))
 
-nb_essai = nb_essai + 1
+        if chiffre_joueur < nombre:
+            print("ton nombre est trop petit",nombre)
 
-if chiffre_joueur < nombre:
-    print("ton nombre est trop petit javais choisi", nombre)
 
-if chiffre_joueur > nombre:
-    print("ton nombre est trop grand javais choisi", nombre)
+        if chiffre_joueur > nombre:
+            print("ton nombre est trop grand")
 
-if chiffre_joueur == nombre:
-    print("bonne reponse javais choisi", nombre)
+
+        if chiffre_joueur == nombre:
+            print("bonne reponse")
+            print()
+            replay = input("voulez vous rejouer? oui ou non?  ")
+            if replay == "non":
+                boucle_jeu = False
+                boucle_essai = False
+
 
 
